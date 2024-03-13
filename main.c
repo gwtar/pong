6c44d0fd5577838453baad1679072c0ff3be9807
gwtar <notkoishi@gmail.com>
2024-03-13 12:30:49 +0800

    todo: math(I'm a math genius)

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ typedef struct
     int speedy;
     int radius;
     Color color;
+    int center;
 } ball;
 
 typedef struct
@@ -33,11 +34,37 @@ void init(ball *ball)
     ball->speedy = 10;
     ball->radius = 5;
     ball->color = WHITE;
+    ball->center = ball->radius / 2;
 }
 
 void logic(ball *ball)
 {
+    /*
+    if(ball->posx >= WINDOW_WIDTH - ball->center)
+    {
+        ball->posx = WINDOW_WIDTH - ball->center;
+        ball->speedx *= -1;
+    }
+    if(ball->posx <= 0 + ball->center)
+    {
+        ball->posx = 0 + ball->center;
+        ball->speedx *= -1;
+    }
+    */
+
+    if(ball->posy >= WINDOW_HEIGHT - ball->center)
+    {
+        ball->posy = WINDOW_HEIGHT - ball->center;
+        ball->speedy *= -1;
+    }
+    if(ball->posy <= 0 + ball->center)
+    {
+        ball->posy = 0 + ball->center;
+        ball->speedy *= -1;
+    }
+
     ball->posx += ball->speedx;
+    ball->posy += ball->speedy;
 }
 
 void draw(ball ball)
