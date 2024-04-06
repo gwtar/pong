31d97c03c0b27a569cc9a015aab81029f4b63aad
gwtar <notkoishi@gmail.com>
2024-04-06 12:10:13 +0800

    cleaned now focusing on adding feature

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #define WINDOW_TITLE "pong"
 #define WINDOW_MAX_FPS 60
 
-typedef struct
+typedef struct ball
 {
     int posx;
     int posy;
@@ -16,12 +16,12 @@ typedef struct
     Color color;
 } ball;
 
-typedef struct
+typedef struct player
 {
     int posx;
     int posy;
-    int sizex;
-    int sizey;
+    int width;
+    int height;
     Color color;
 } player;
 
@@ -30,52 +30,51 @@ void init(ball *ball, player *player)
 
     ball->posx = WINDOW_WIDTH / 2;
     ball->posy = WINDOW_HEIGHT / 2;
-    ball->speedx = 11;
-    ball->speedy = 10;
+    ball->speedx = 5;
+    ball->speedy = 1;
     ball->radius = 5;
     ball->color = WHITE;
 
     player->posx = 0;
     player->posy = 0;
-    player->sizex = 10;
-    player->sizey = 100;
+    player->width = 10;
+    player->height = 100;
     player->color = WHITE;
 }
 
 void logic(ball *ball, player *player)
 {
-    if(ball->posx > WINDOW_WIDTH)
+    if(ball->posx + ball->radius > WINDOW_WIDTH)
     {
-        ball->posx = WINDOW_WIDTH;
+        ball->posx = WINDOW_WIDTH - ball->radius;
         ball->speedx *= -1;
     }
-    if(ball->posx <= 0 + ball->center)
+    if(ball->posx - ball->radius < 0)
     {
-        ball->posx = 0 + ball->center;
+        ball->posx = 0 + ball->radius;
         ball->speedx *= -1;
     }
 
-    if(ball->posy >= WINDOW_HEIGHT - ball->center)
+    if(ball->posy + ball->radius > WINDOW_HEIGHT)
     {
-        ball->posy = WINDOW_HEIGHT - ball->center;
+        ball->posy = WINDOW_HEIGHT - ball->radius;
         ball->speedy *= -1;
     }
-    if(ball->posy <= 0 + ball->center)
+    if(ball->posy - ball->radius < 0)
     {
-        ball->posy = 0 + ball->center;
+        ball->posy = 0 + ball->radius;
         ball->speedy *= -1;
     }
 
     ball->posx += ball->speedx;
     ball->posy += ball->speedy;
 
-    player->posy = GetMouseY() - player->centery;
+    player->posy = GetMouseY() - player->height / 2;
 
-    if(player->posy > WINDOW_HEIGHT - player->sizey)
+    if(player->posy + player->height > WINDOW_HEIGHT)
     {
-        player->posy = WINDOW_HEIGHT - player->sizey;
+        player->posy = WINDOW_WIDTH - player->height;
     }
-
     if(player->posy < 0)
     {
         player->posy = 0;
@@ -88,7 +87,7 @@ void draw(ball ball, player player)
     ClearBackground(BLACK);
 
     DrawCircle(ball.posx, ball.posy, ball.radius, ball.color);
-    DrawRectangle(player.posx, player.posy, player.sizex, player.sizey, player.color);
+    DrawRectangle(player.posx, player.posy, player.width, player.height, player.color);
 
     EndDrawing();
 }
